0524465fd012f022819d9427519b17f165f5ccbc
MCharron93 <michelle.charron93@gmail.com>
2021-02-15 11:33:40 -0700

    Card edits

--- src/components/Headers/LandingHeader.js
+++ src/components/Headers/LandingHeader.js
@@ -13,53 +13,31 @@ const LandingHeader = () => {
             <Row>
               <Col lg="6" xl="3">
                 <Card className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
                         <CardTitle
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
                           Changes
                         </CardTitle>
-                  {/* <CardBody>
-                    <Row>
-                      <div className="col">
-                        <span className="h2 font-weight-bold mb-0">
-                          350,897
-                        </span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                          <i className="ni ni-air-baloon" />
-                        </div>
-                      </Col>
-                    </Row>
-                    <p className="mt-3 mb-0 text-muted text-sm">
+                        <p className="mt-3 mb-0 text-muted text-sm">
                       <span className="text-success mr-2">
                         <i className="ni ni-basket" /> 3.48%
                       </span>{" "}
                       <span className="text-nowrap">Since last month</span>
-                    </p>
-                  </CardBody> */}
+                    </p>  
+                        </CardBody>
                 </Card>
               </Col>
               <Col lg="6" xl="3">
                 <Card className="card-stats mb-4 mb-xl-0">
                   <CardBody>
-                    <Row>
-                      <div className="col">
                         <CardTitle
                           tag="h5"
                           className="text-uppercase text-muted mb-0"
                         >
-                          All Users
+                          Changes
                         </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">2,356</span>
-                      </div>
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
-                          <i className="ni ni-briefcase-24" />
-                        </div>
-                      </Col>
-                    </Row>
                     <p className="mt-3 mb-0 text-muted text-sm">
                       <span className="text-danger mr-2">
                         <i className="fas fa-arrow-down" /> 3.48%
