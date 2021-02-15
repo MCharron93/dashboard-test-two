8d9f32415d739d5424838566314966789699c74f
MCharron93 <michelle.charron93@gmail.com>
2021-02-15 11:05:21 -0700

    Rearranging the grid

--- src/views/my-views/Landing.js
+++ src/views/my-views/Landing.js
@@ -21,12 +21,12 @@ import {
 const Landing = () => {
     return(
         <>
-        <Header>
-        <Container className="bg-primary">
-            <Row className="m-5">
-            <Col>
-                <Card>
-                    <CardHeader>
+        <Header />
+        <Container className="mt--7" fluid>
+        <Row>
+          <div className="col">
+            <Card className="shadow border-0">
+            <CardHeader>
                         <div>
                             <h4>
                                 Hello Nerds!
@@ -40,6 +40,53 @@ const Landing = () => {
                             </p>
                         </div>
                     </CardBody>
+            </Card>
+          </div>
+          <div className="col">
+          <Card className="shadow border-0">
+            <CardHeader>
+                        <div>
+                            <h4>
+                                Hello Nerds!
+                            </h4>
+                       </div> 
+                    </CardHeader>
+                    <CardBody>
+                        <div>
+                            <p>
+                                This is just a test for creating a card.
+                            </p>
+                        </div>
+                    </CardBody>
+            </Card>
+          </div>
+        </Row>
+        <Row>
+        <div className="col my-4">
+            <Card className="shadow border-0">
+            <CardHeader>
+                        <div>
+                            <h4>
+                                Hello Nerds!
+                            </h4>
+                       </div> 
+                    </CardHeader>
+                    <CardBody>
+                        <div>
+                            <p>
+                                This is just a test for creating a card.
+                            </p>
+                        </div>
+                    </CardBody>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+        <Container className="bg-primary">
+            <Row className="m-5">
+            <Col>
+                <Card>
+                   
                 </Card>
             </Col>
             <Col>
@@ -80,7 +127,6 @@ const Landing = () => {
             </Col>
             </Row>
         </Container>
-        </Header>
         </>
     )
 }
