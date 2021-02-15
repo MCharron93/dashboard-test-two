fd0909d5c41755ac7bdb759e3dd584f1be345885
MCharron93 <michelle.charron93@gmail.com>
2021-02-15 16:43:23 -0700

    Remove register section

--- src/routes.js
+++ src/routes.js
@@ -18,7 +18,7 @@
 import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
 import Maps from "views/examples/Maps.js";
-import Register from "views/examples/Register.js";
+// import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
@@ -68,13 +68,13 @@ var routes = [
     component: Login,
     layout: "/auth",
   },
-  {
-    path: "/register",
-    name: "Register",
-    icon: "ni ni-circle-08",
-    component: Register,
-    layout: "/auth",
-  },
+  // {
+  //   path: "/register",
+  //   name: "Register",
+  //   icon: "ni ni-circle-08",
+  //   component: Register,
+  //   layout: "/auth",
+  // },
   {
     path: "/landing",
     name: "Landing",
