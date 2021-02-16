be21b6f8b765ada1f95e75828d49fcbc573c97d7
MCharron93 <michelle.charron93@gmail.com>
2021-02-16 15:17:46 -0700

    Try creating tools header for customization

--- src/views/my-views/Tools.js
+++ src/views/my-views/Tools.js
@@ -27,10 +27,11 @@ class Tools extends React.Component {
       };
 render() {
     return (
-        <React.Fragment>
+      <React.Fragment>
+          <div className="header bg-gradient-default pb-4 pt-5 pt-md-">
         <div className="nav-wrapper">
           <Nav
-            className="nav-fill flex-column flex-md-row mx-3"
+            className="nav-fill flex-column flex-md-row mx-4"
             id="tabs-icons-text"
             pills
             role="tablist"
@@ -92,9 +93,38 @@ render() {
                 Fulfillment
               </NavLink>
             </NavItem>
+            <NavItem>
+              <NavLink
+                aria-selected={this.state.tabs === 5}
+                className={classnames("mb-sm-3 mb-md-0", {
+                  active: this.state.tabs === 5
+                })}
+                onClick={e => this.toggleNavs(e, "tabs", 5)}
+                href="#pablo"
+                role="tab"
+              >
+                <i className="ni ni-cloud-upload-96 mr-2" />
+                Shipping
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                aria-selected={this.state.tabs === 6}
+                className={classnames("mb-sm-3 mb-md-0", {
+                  active: this.state.tabs === 6
+                })}
+                onClick={e => this.toggleNavs(e, "tabs", 6)}
+                href="#pablo"
+                role="tab"
+              >
+                <i className="ni ni-cloud-upload-96 mr-2" />
+                Analytics
+              </NavLink>
+            </NavItem>
           </Nav>
         </div>
-        <Card className="shadow">
+        </div>
+        <Card className="shadow mx-4 rounded">
           <CardBody>
               {/* NOTE this changes the state of the tabs and sets it to display whatever tab is currently selected */}
             <TabContent activeTab={"tabs" + this.state.tabs}>
@@ -128,6 +158,23 @@ render() {
                 </p>
                 <p>
                 Some icons and other clickable links will launch applications and other windows for everyone to use for any fulfillment needs
+                </p>
+              </TabPane>
+              <TabPane tabId="tabs5">
+              <p className="description">
+                This would be Shipping tools injected here. Can include logos and other applications used by the team for completing thier jobs.
+                </p>
+                <p>
+                Some icons and other clickable links will launch applications and other windows for everyone to use for any Shipping needs
+                </p>
+              </TabPane>
+              <TabPane tabId="tabs6">
+                  <h4>This section might not be needed in the future due to pulling in Google Analytics onto the Dashboard </h4>
+              <p className="description">
+                This would be Analytics tools injected here. Can include logos and other applications used by the team for completing thier jobs.
+                </p>
+                <p>
+                Some icons and other clickable links will launch applications and other windows for everyone to use for any Analytics needs
                 </p>
               </TabPane>
             </TabContent>
