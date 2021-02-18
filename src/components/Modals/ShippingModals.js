e4efa2dded79b0f0bfa624fb8dce51f92ee2e8f8
MCharron93 <michelle.charron93@gmail.com>
2021-02-18 13:10:44 -0700

    Alt shippind method modals set

--- src/components/Modals/ShippingModals.js
+++ src/components/Modals/ShippingModals.js
@@ -162,6 +162,152 @@ class ShippingModals extends React.Component {
               </div>
             </Modal>
           </Col>
+          <Col md="6">
+          <h5 className="btn shadow-none text-white" id="custom-btn" onClick={() => this.toggleModal("fedex")}>&#8226; &nbsp;&nbsp;FedEx</h5>
+            <Modal
+              className="modal-dialog-centered"
+              size="lg"
+              isOpen={this.state.fedex}
+              toggle={() => this.toggleModal("fedex")}
+            >
+              <div className="modal-header">
+                <h2 className="modal-title" id="modal-title-default">
+                 FedEx
+                </h2>
+                <button
+                  aria-label="Close"
+                  className="close"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("fedex")}
+                >
+                  <span aria-hidden={true}>×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <h5>How do you submit a claim through fedex?</h5>
+                <p>
+                &#8226; &nbsp;&nbsp;We have to do it, not the customer
+                <br/>
+                &#8226; &nbsp;&nbsp;fedex will ask for prices of each item in the package; if all the prices are different calculate an average 
+                &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;price
+                <br/>
+                &#8226; &nbsp;&nbsp;They will want screenshots of the order as proof
+                <br/>
+                &#8226; &nbsp;&nbsp;Give them personal phone number for contact. Contact Darcie if needed.
+                </p>
+
+                <h5>
+                Why is fedex not available as a shipping option? 
+                </h5>
+                <p>
+                Check if there are any Vape Pens in their order - fedex will not ship Vape Pens. 
+                <br/>Place a mock order on the Happy Hippo Website - If fedex is not an option their address is undeliverable generally due to the address being too rural.
+                </p>
+
+                <h5>Checking Exceptions:</h5>
+                <p>Log into Quantum View. Go to Quantum view manage: From here to can view exceptions, change address and return to sender. fedex exceptions only need to 
+                  be addressed if there is an address issue.</p>
+                
+                <h5>Missing Packages</h5>
+                <p>&#8226; &nbsp;&nbsp;Lookup package details using Quantum View. 
+                <br/>
+                &#8226; &nbsp;&nbsp;If a fedex package is lost, you will file a claim and ship a replacement package.
+                <br/>
+                &#8226; &nbsp;&nbsp;If a package has been marked as “Delivered” - ask the customer if they have checked around the Front Door and lawn.
+                <br/>
+                &#8226; &nbsp;&nbsp;Ask them if they have checked with other residents and neighbors who may have received the package on their behalf.
+                <br/>
+                &#8226; &nbsp;&nbsp;If they have done these things we can File a Claim and ship them a replacement package, have them verify their shipping address to ensure it is going to the correct address.
+                <br/>
+                &#8226; &nbsp;&nbsp;In certain cases we can reach out to Caroline, our FedEx Rep, and have her help locate packages or give us more information.</p>
+              </div>
+              <div className="modal-footer">
+                <Button
+                  className="ml-auto"
+                  color="default"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("fedex")}
+                >
+                  Close
+                </Button>
+              </div>
+            </Modal>
+          </Col>
+          <Col md="6">
+          <h5 className="btn shadow-none text-white" id="custom-btn" onClick={() => this.toggleModal("usps")}>&#8226; &nbsp;&nbsp;USPS</h5>
+            <Modal
+              className="modal-dialog-centered"
+              size="lg"
+              isOpen={this.state.usps}
+              toggle={() => this.toggleModal("usps")}
+            >
+              <div className="modal-header">
+                <h2 className="modal-title" id="modal-title-default">
+                 USPS
+                </h2>
+                <button
+                  aria-label="Close"
+                  className="close"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("usps")}
+                >
+                  <span aria-hidden={true}>×</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <h5>How do you submit a claim through usps?</h5>
+                <p>
+                &#8226; &nbsp;&nbsp;We have to do it, not the customer
+                <br/>
+                &#8226; &nbsp;&nbsp;usps will ask for prices of each item in the package; if all the prices are different calculate an average 
+                &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;price
+                <br/>
+                &#8226; &nbsp;&nbsp;They will want screenshots of the order as proof
+                <br/>
+                &#8226; &nbsp;&nbsp;Give them personal phone number for contact. Contact Darcie if needed.
+                </p>
+
+                <h5>
+                Why is usps not available as a shipping option? 
+                </h5>
+                <p>
+                Check if there are any Vape Pens in their order - usps will not ship Vape Pens. 
+                <br/>Place a mock order on the Happy Hippo Website - If usps is not an option their address is undeliverable generally due to the address being too rural.
+                </p>
+
+                <h5>Checking Exceptions:</h5>
+                <p>Log into Quantum View. Go to Quantum view manage: From here to can view exceptions, change address and return to sender. usps exceptions only need to 
+                  be addressed if there is an address issue.</p>
+                
+                <h5>Missing Packages</h5>
+                <p>&#8226; &nbsp;&nbsp;Lookup package details using Quantum View. 
+                <br/>
+                &#8226; &nbsp;&nbsp;If a usps package is lost, you will file a claim and ship a replacement package.
+                <br/>
+                &#8226; &nbsp;&nbsp;If a package has been marked as “Delivered” - ask the customer if they have checked around the Front Door and lawn.
+                <br/>
+                &#8226; &nbsp;&nbsp;Ask them if they have checked with other residents and neighbors who may have received the package on their behalf.
+                <br/>
+                &#8226; &nbsp;&nbsp;If they have done these things we can File a Claim and ship them a replacement package, have them verify their shipping address to ensure it is going to the correct address.
+                <br/>
+                &#8226; &nbsp;&nbsp;In certain cases we can reach out to Caroline, our FedEx Rep, and have her help locate packages or give us more information.</p>
+              </div>
+              <div className="modal-footer">
+                <Button
+                  className="ml-auto"
+                  color="default"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => this.toggleModal("usps")}
+                >
+                  Close
+                </Button>
+              </div>
+            </Modal>
+          </Col>
       </>
     );
   }
