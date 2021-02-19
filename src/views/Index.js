26b2a5d444c268325519d631f1e44025550ab3fd
MCharron93 <michelle.charron93@gmail.com>
2021-02-19 15:20:41 -0700

    Reviews section

--- src/views/Index.js
+++ src/views/Index.js
@@ -143,14 +143,18 @@ const Index = (props) => {
         </Row>
         <Row className="mt-5 my-4">
           <Col className="mb-5 mb-xl-0" xl="8">
-            <Card className="shadow">
+            <Card className="shadow mt--6">
               <CardHeader className="border-0">
                 <Row className="align-items-center">
                   <div className="col">
-                    <h3 className="mb-0">Page visits</h3>
+                  <h6 className="text-uppercase text-muted ls-1 mb-1">
+                      Customer Recognition
+                    </h6>
+                    <h3 className="mb-0 display-3">Hippo Hype</h3>
                   </div>
                   <div className="col text-right">
                     <Button
+                      className="rounded"
                       color="primary"
                       href="#pablo"
                       onClick={(e) => e.preventDefault()}
@@ -161,171 +165,42 @@ const Index = (props) => {
                   </div>
                 </Row>
               </CardHeader>
-              <Table className="align-items-center table-flush" responsive>
-                <thead className="thead-light">
-                  <tr>
-                    <th scope="col">Page name</th>
-                    <th scope="col">Visitors</th>
-                    <th scope="col">Unique users</th>
-                    <th scope="col">Bounce rate</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">/argon/</th>
-                    <td>4,569</td>
-                    <td>340</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-success mr-3" /> 46,53%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/index.html</th>
-                    <td>3,985</td>
-                    <td>319</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/charts.html</th>
-                    <td>3,513</td>
-                    <td>294</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      36,49%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/tables.html</th>
-                    <td>2,050</td>
-                    <td>147</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-success mr-3" /> 50,87%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/profile.html</th>
-                    <td>1,795</td>
-                    <td>190</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-danger mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
+              <CardBody>
+              <Row>
+  <Col sm="3">Jenny Orlando via TrustPilot</Col>
+  <Col sm="9">
+    <a href="https://www.trustpilot.com/review/www.happyhippoherbals.com">
+    <p>
+      Vestibulum id ligula porta felis euismod semper eget lacinia
+      odio sem nec elit.
+    </p>
+    </a>
+  </Col>
+  <Col sm="3">Linda Callahan via KratomIQ</Col>
+  <Col sm="9">
+    <a href="https://www.kratomiq.com/happy-hippo-herbals-review/">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Dolorum temporibus itaque ratione cum harum distinctio totam esse 
+      aspernatur saepe blanditiis voluptatem, tempore, vel id dignissimos 
+      veritatis eveniet reiciendis nemo qui!
+    </p>
+    </a>
+  </Col>
+  <Col sm="3">Jennifer Kurtz via kratomguides</Col>
+  <Col sm="9">
+    <a href="https://kratomguides.com/happy-hippo-herbal-reviews/">
+    <p>
+      Dolorum temporibus itaque ratione cum harum distinctio totam esse 
+      aspernatur saepe blanditiis voluptatem, tempore, vel id dignissimos 
+      veritatis eveniet reiciendis nemo qui! 
+    </p>
+    </a>
+  </Col>
+</Row>
+              </CardBody>
             </Card>
           </Col>
-          {/* <Col xl="4">
-            <Card className="shadow">
-              <CardHeader className="border-0">
-                <Row className="align-items-center">
-                  <div className="col">
-                    <h3 className="mb-0">Social traffic</h3>
-                  </div>
-                  <div className="col text-right">
-                    <Button
-                      color="primary"
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                      size="sm"
-                    >
-                      See all
-                    </Button>
-                  </div>
-                </Row>
-              </CardHeader>
-              <Table className="align-items-center table-flush" responsive>
-                <thead className="thead-light">
-                  <tr>
-                    <th scope="col">Referral</th>
-                    <th scope="col">Visitors</th>
-                    <th scope="col" />
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">Facebook</th>
-                    <td>1,480</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">60%</span>
-                        <div>
-                          <Progress
-                            max="100"
-                            value="60"
-                            barClassName="bg-gradient-danger"
-                          />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Facebook</th>
-                    <td>5,480</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">70%</span>
-                        <div>
-                          <Progress
-                            max="100"
-                            value="70"
-                            barClassName="bg-gradient-success"
-                          />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Google</th>
-                    <td>4,807</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">80%</span>
-                        <div>
-                          <Progress max="100" value="80" />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Instagram</th>
-                    <td>3,678</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">75%</span>
-                        <div>
-                          <Progress
-                            max="100"
-                            value="75"
-                            barClassName="bg-gradient-info"
-                          />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">twitter</th>
-                    <td>2,645</td>
-                    <td>
-                      <div className="d-flex align-items-center">
-                        <span className="mr-2">30%</span>
-                        <div>
-                          <Progress
-                            max="100"
-                            value="30"
-                            barClassName="bg-gradient-warning"
-                          />
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Card>
-          </Col> */}
         </Row>
       </Container>
     </>
