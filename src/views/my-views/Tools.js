bf1a5401c2d5f838388737144655ec28013811bf
MCharron93 <michelle.charron93@gmail.com>
2021-02-22 11:01:38 -0700

    imported cust. service tools

--- src/views/my-views/Tools.js
+++ src/views/my-views/Tools.js
@@ -323,6 +323,22 @@ render() {
               </div>
               </a>
             </div>
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
+              <a href="https://login.blockchain.com/#/login" target="_blank" rel="noopener noreferrer">
+                <img className="w-25" src="https://walletscrutiny.com/images/wallet_icons/android/piuk.blockchain.android.png" alt="instagram"/>
+              <div className="">
+                 Blockchain
+              </div>
+              </a>
+            </div>
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
+              <a href="https://accounts.zoho.com/signin?servicename=ZohoBooks&signupurl=https://www.zoho.com/us/books/signup/" target="_blank" rel="noopener noreferrer">
+                <img className="w-25" src="https://www.zoho.com/branding/images/zoho-logo-512px.png" alt="instagram"/>
+              <div className="pt-4">
+                 Zoho
+              </div>
+              </a>
+            </div>
               </div>
             </TabPane>
             <TabPane tabId="tabs5">
