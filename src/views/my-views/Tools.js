1f26811f5f070de7dbea613fde8b0a72ca5dc91f
MCharron93 <michelle.charron93@gmail.com>
2021-02-22 10:46:24 -0700

    padding edits to tool items

--- src/views/my-views/Tools.js
+++ src/views/my-views/Tools.js
@@ -48,7 +48,7 @@ render() {
               href="#pablo"
               role="tab"
             >
-              <i className="ni ni-cloud-upload-96 mr-2" />
+              <i className="ni ni-cloud-upload-96 " />
               General Business
             </NavLink>
           </NavItem>
@@ -62,7 +62,7 @@ render() {
               href="#pablo"
               role="tab"
             >
-              <i className="ni ni-cloud-upload-96 mr-2" />
+              <i className="ni ni-cloud-upload-96 " />
               Google Suite
             </NavLink>
           </NavItem>
@@ -76,7 +76,7 @@ render() {
               href="#pablo"
               role="tab"
             >
-              <i className="ni ni-bell-55 mr-2" />
+              <i className="ni ni-bell-55 " />
               Design
             </NavLink>
           </NavItem>
@@ -90,7 +90,7 @@ render() {
               href="#pablo"
               role="tab"
             >
-              <i className="ni ni-calendar-grid-58 mr-2" />
+              <i className="ni ni-calendar-grid-58 " />
               Development
             </NavLink>
           </NavItem>
@@ -104,7 +104,7 @@ render() {
               href="#pablo"
               role="tab"
             >
-              <i className="ni ni-cloud-upload-96 mr-2" />
+              <i className="ni ni-cloud-upload-96" />
               Fulfillment
             </NavLink>
           </NavItem>
@@ -118,7 +118,7 @@ render() {
               href="#pablo"
               role="tab"
             >
-              <i className="ni ni-cloud-upload-96 mr-2" />
+              <i className="ni ni-cloud-upload-96" />
               Sales
             </NavLink>
           </NavItem>
@@ -132,7 +132,7 @@ render() {
               href="#pablo"
               role="tab"
             >
-              <i className="ni ni-cloud-upload-96 mr-2" />
+              <i className="ni ni-cloud-upload-96" />
               Research and Development
             </NavLink>
           </NavItem>
@@ -147,8 +147,8 @@ render() {
             {/* NOTE consider creating the tab content as a child that passes props up to parent component. This file will get too large soon with all of the data for each department */}
           <TabContent activeTab={"tabs" + this.state.tabs}>
           <TabPane tabId="tabs1">
-            <div className="row justify-content-around text-center px-2">
-            <div className="col-3 d-flex align-items-center">
+            <div className="row justify-content-around text-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://app.asana.com/0/home/1199896929625869" target="_blank" rel="noopener noreferrer">
                 <img className="w-25" src="https://static.wixstatic.com/media/2d437d_951ac25396fc43e5932dfe54f6556bc3~mv2.png/v1/fill/w_500,h_370,al_c,q_85,usm_0.66_1.00_0.01/asana-logo-logo.webp" alt="instagram"/>
               <div className="pt-2">
@@ -156,47 +156,49 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://www.happyhippoherbals.com/admin" target="_blank" rel="noopener noreferrer">
-                <img className="w-25" src="https://i0.wp.com/res.cloudinary.com/geoxis/images/f_auto,q_auto/v1609829844/App-logo-opencart-uai-258x258/App-logo-opencart-uai-258x258.png?ssl=1" alt="gmail"/>
+                <img className="w-25" src="https://i0.wp.com/res.cloudinary.com/geoxis/images/f_auto,q_auto/v1609829844/App-logo-opencart-uai-258x258/App-logo-opencart-uai-258x258.png?ssl=1" alt="OpenCart"/>
               <div className="pt-2">
                   OpenCart
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://www.semrush.com/dashboard/" target="_blank" rel="noopener noreferrer">
-                <img className="w-50" src="https://financesonline.com/uploads/2020/12/semrush-logo-1.png" alt="instagram"/>
+                <img className="w-50" src="https://financesonline.com/uploads/2020/12/semrush-logo-1.png" alt="SEMRush"/>
               <div className="pt-2">
                   SEMRush
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://app.slack.com/client/TM2S3EWA1/CLRDZAXSP" target="_blank" rel="noopener noreferrer">
-                <img className="w-25" src="https://marketing-assets.pinpointhq.com/wp-content/uploads/2020/04/pinpoint-integration-slack-logo-63a5b836f54577f6ad9f3c8a700a30a9.png" alt="instagram"/>
+                <img className="w-25" src="https://marketing-assets.pinpointhq.com/wp-content/uploads/2020/04/pinpoint-integration-slack-logo-63a5b836f54577f6ad9f3c8a700a30a9.png" alt="slack"/>
               <div className="pt-2">
                  Slack
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://speciosa.org/home/kratom-legality-map/" target="_blank" rel="noopener noreferrer">
-                <img className="w-50" src="https://speciosa.org/wp-content/uploads/2017/12/Untitled-1-01.png" alt="instagram"/>
+                <img className="w-50" src="https://speciosa.org/wp-content/uploads/2017/12/Untitled-1-01.png" alt="map"/>
               <div className="pt-2">
                   Kratom Legality 
               </div>
               </a>
             </div>
-            {/* <div className="col-3 d-flex align-items-center">
-              <a href="https://docs.google.com/document/u/0/?tgif=d" target="_blank" rel="noopener noreferrer">
-                <img className="w-25" src="https://1000logos.net/wp-content/uploads/2020/05/Google-Docs-logo.png" alt="gmail"/>
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
+              <a href="https://www.happyhippoherbals.com/" target="_blank" rel="noopener noreferrer">
+                <img className="w-25" src={
+                require("../../assets/img/brand/happy-hippo-logo-no-text.png").default
+              } alt="happyhippo"/>
               <div className="pt-2">
-                  Docs
+                  Happy Hippo Herbals
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            {/* <div className="col-3 d-flex align-items-center justify-content-center">
               <a href="https://docs.google.com/spreadsheets/u/0/?tgif=d" target="_blank" rel="noopener noreferrer">
                 <img className="w-50" src="https://cdn.buttercms.com/SMsY0FLvTlm0TRINX6WH" alt="gmail"/>
               <div className="pt-2">
@@ -204,7 +206,7 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center">
               <a href="https://docs.google.com/presentation/u/0/?tgif=d" target="_blank" rel="noopener noreferrer">
                 <img className="w-25" src="https://3.bp.blogspot.com/-Xgy8k-BxOfg/WgW4jipZLsI/AAAAAAAAulA/kRs0H-cGFXkIsdql6TMunGY5fzY4ZP4NgCK4BGAYYCw/s1600/google_slides1600.png" alt="instagram"/>
               <div className="pt-2">
@@ -216,7 +218,7 @@ render() {
             </TabPane>
             <TabPane tabId="tabs2">
             <div className="row justify-content-around text-center px-2">
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://mail.google.com/mail/u/0/#inbox" target="_blank" rel="noopener noreferrer">
                 <img className="w-25" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/512px-Gmail_icon_%282020%29.svg.png" alt="gmail"/>
               <div className="pt-2">
@@ -224,7 +226,7 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://calendar.google.com/calendar/u/0/r" target="_blank" rel="noopener noreferrer">
                 <img className="w-50" src="https://www.clio.com/wp-content/uploads/2018/07/Calendar-400x267.png" alt="instagram"/>
               <div className="pt-2">
@@ -233,7 +235,7 @@ render() {
               </a>
             </div>
             
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://drive.google.com/drive/my-drive" target="_blank" rel="noopener noreferrer">
                 <img className="w-50" src="https://ssl.gstatic.com/images/branding/product/2x/hh_drive_96dp.png" alt="instagram"/>
               <div className="pt-2">
@@ -241,7 +243,7 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://docs.google.com/document/u/0/?tgif=d" target="_blank" rel="noopener noreferrer">
                 <img className="w-25" src="https://1000logos.net/wp-content/uploads/2020/05/Google-Docs-logo.png" alt="gmail"/>
               <div className="pt-2">
@@ -249,7 +251,7 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://docs.google.com/spreadsheets/u/0/?tgif=d" target="_blank" rel="noopener noreferrer">
                 <img className="w-50" src="https://cdn.buttercms.com/SMsY0FLvTlm0TRINX6WH" alt="gmail"/>
               <div className="pt-2">
@@ -257,7 +259,7 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://docs.google.com/presentation/u/0/?tgif=d" target="_blank" rel="noopener noreferrer">
                 <img className="w-25" src="https://3.bp.blogspot.com/-Xgy8k-BxOfg/WgW4jipZLsI/AAAAAAAAulA/kRs0H-cGFXkIsdql6TMunGY5fzY4ZP4NgCK4BGAYYCw/s1600/google_slides1600.png" alt="instagram"/>
               <div className="pt-2">
@@ -269,7 +271,7 @@ render() {
             </TabPane>
             <TabPane tabId="tabs3">
             <div className="row justify-content-around text-center px-2">
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://stock.adobe.com/" target="_blank" rel="noopener noreferrer">
                 <img className="w-50" src="https://lh3.googleusercontent.com/proxy/qmW5fXUZb9dfeZMlwIUIKsZMlh9fBZMY2xpTSu9eHk72pcNdZ_dGKrs60wP3Op2CxNU6WRo6cYwgJEA0sgWQiyGSaL6N2AUuGhKkSWJKf9l7iz3wrOI" alt="stock images"/>
               <div className="pt-2">
@@ -277,7 +279,7 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://www.adobe.com/products/photoshop.html" target="_blank" rel="noopener noreferrer">
                 <img className="w-25" src="https://cdn3.iconfinder.com/data/icons/logos-brands-3/24/logo_brand_brands_logos_adobe_photoshop-512.png" alt="gmail"/>
               <div className="pt-2">
@@ -285,7 +287,7 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://www.adobe.com/products/illustrator.html" target="_blank" rel="noopener noreferrer">
                 <img className="w-25" src="https://logodownload.org/wp-content/uploads/2017/04/adobe-Illustrator-logo-3.png" alt="gmail"/>
               <div className="pt-2">
@@ -293,7 +295,7 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://www.instagram.com/realhappyhippoherbals/" target="_blank" rel="noopener noreferrer">
                 <img className="w-25" src="https://instagram-brand.com/wp-content/uploads/2016/11/Instagram_AppIcon_Aug2017.png?w=00" alt="instagram"/>
               <div className="pt-2">
@@ -301,7 +303,7 @@ render() {
               </div>
               </a>
             </div>
-            <div className="col-3 d-flex align-items-center">
+            <div className="col-3 d-flex align-items-center justify-content-center justify-content-center py-4">
               <a href="https://www.adobe.com/products/xd.html" target="_blank" rel="noopener noreferrer">
                 <img className="w-25" src="https://cdn.freebiesupply.com/logos/thumbs/2x/adobe-xd-logo.png" alt="instagram"/>
               <div className="pt-2">
