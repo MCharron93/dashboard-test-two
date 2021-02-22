9448b8cd3a790658945dd7ff2c2f744498d3f6e0
MCharron93 <michelle.charron93@gmail.com>
2021-02-22 12:04:27 -0700

    Create blog post link and snippet

--- src/views/my-views/Landing.js
+++ src/views/my-views/Landing.js
@@ -27,35 +27,53 @@ const Landing = () => {
         <Row>
           <div className="col">
             <Card className="shadow border-0">
-            <CardHeader>
-                        <div>
-                            <h4>
-                                Blog Title!
-                            </h4>
-                       </div> 
-                    </CardHeader>
-                    <CardBody>
-                        <div>
-                            <p>
-                                This is just a test for creating a card.
+                <CardHeader>
+                    <div>
+                        <h5 className="text-uppercase text-muted ls-1 mb-1">
+                            Updated Blog Posts
+                        </h5>
+                        <h4 className="display-4">
+                            Using Bitcoin Payment
+                        </h4>
+                    </div> 
+                </CardHeader>
+                <CardBody>
+                    <a href="https://www.happyhippoherbals.com/blog/using-bitcoin-payment-method-not-intimidating" target="_blank" rel="noreferrer">
+                        <div  className="d-flex"> 
+                            <img className="rounded card-img col-6" src="https://lh3.googleusercontent.com/mX5yY5b-ogMiLXOkE0gxt-MA2juBkbYySJ4OqxWgZq4bovO0bqSxOnzq45fr-CmBc47SbWftbf-_1k4iUnTkLgEd_CJEmR9aLeEe5pr5U2i_9sWqhQkzb1ZiEtGbnoFMeyIEZgA" alt=""/>
+                            <p className="col-6 px-3">
+                            To all our fellow Hippo Customers - we deeply apologize for the issues and inconveniences you have 
+                            experienced with our Credit Card Processors going down. To explain, banks do not enjoy working with 
+                            Kratom companies - in fact they usually refuse. Therefore it is very hard to find a Credit Card Processor 
+                            that is safe and suitable for our company. We have thought about building our own website plugin, 
+                            but things like that take time.We are currently working as hard as we can to find a replacement for 
+                            PMC and our Off-Site Payment Method....
+                            <br/>
+                            <br/>
+                            Read More?
                             </p>
                         </div>
-                    </CardBody>
+                    </a>
+                </CardBody>
             </Card>
           </div>
           <div className="col">
           <Card className="shadow border-0">
             <CardHeader>
                         <div>
-                            <h4>
-                                Hello Nerds!
+                            <h5 className="text-uppercase text-muted ls-1 mb-1">
+                                Site Wide News
+                            </h5>
+                            <h4 className="display-4">
+                                Current Sales and Promos!
                             </h4>
                        </div> 
                     </CardHeader>
                     <CardBody>
                         <div>
-                            {/* NOTE an a tag that pushes to separate window for the IT tickets */}
-                            <a href="https://form.asana.com/?k=qxP8ysQvIY6l5IDI4h9LUg&d=1195144362945755" target="_blank" rel="noreferrer">Here would be a quick snippet of the article that would like to more info </a>
+                            <a href="https://www.happyhippoherbals.com/sales" target="_blank" rel="noreferrer">
+                                
+                            </a>
                         </div>
                     </CardBody>
             </Card>
