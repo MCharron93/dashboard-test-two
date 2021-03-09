9e2aadebd61a25cd118d71cb0c46389249934923
MCharron93 <michelle.charron93@gmail.com>
2021-03-09 16:49:57 -0700

    pulling in photos for logo tiles

--- src/variables/Tile.js
+++ src/variables/Tile.js
@@ -1,18 +1,28 @@
 import { Card } from '@material-ui/core'
 import React from 'react'
-import { CardHeader } from 'reactstrap'
+import { CardBody, CardHeader } from 'reactstrap'
+
+// NOTE img pulled in by instaniating them as consts 
 
 class Icontiles extends React.Component{
     state = {
-        tiles: [ 1, 2, 1, 2, 5, 3, 5]
+        tiles: [
+            {
+                name: "Asana",
+                img: "../assets/img/Icon Tiles/asana-logo.png"
+            }
+        ]
     }
 
     render(){
         return(
             <Card>
                 <CardHeader>
-                    <h1>Hello</h1>
+                    <h1>{this.state.tiles.name}</h1>
                 </CardHeader>
+                    <CardBody>
+                        <img src={this.state.tiles.img} alt="this didn't work"></img>
+                    </CardBody>
             </Card>
         )
     }
