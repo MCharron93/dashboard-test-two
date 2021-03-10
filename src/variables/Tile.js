c4109e39ae14f9cbd4ed81068f9fe2898ddd9435
MCharron93 <michelle.charron93@gmail.com>
2021-03-10 10:02:44 -0700

    formatting for reusable card drawing

--- src/variables/Tile.js
+++ src/variables/Tile.js
@@ -2,27 +2,46 @@ import { Card } from '@material-ui/core'
 import React from 'react'
 import { CardBody, CardHeader } from 'reactstrap'
 
-// NOTE img pulled in by instaniating them as consts 
+// NOTE img pulled in by instaniating them as consts
+const devTools = {
+    tiles: [
+        {
+            name: "Asana",
+            img: "../assets/img/Icon Tiles/asana-logo.png"
+        }
+    ]
+} 
 
 class Icontiles extends React.Component{
-    state = {
-        tiles: [
-            {
-                name: "Asana",
-                img: "../assets/img/Icon Tiles/asana-logo.png"
-            }
-        ]
-    }
+    // state = {
+    //     tiles: [
+    //         {
+    //             name: "Asana",
+    //             img: "../assets/img/Icon Tiles/asana-logo.png"
+    //         },
+    //         {   
+    //             name: "Instagram",
+    //             img: "../assets/img/Icon Tiles/instagram-logo.png"
+    //         }
+    //     ]
+    // }
 
     render(){
         return(
             <Card>
                 <CardHeader>
-                    <h1>{this.state.tiles.name}</h1>
+                    <h2>{this.props.name}</h2>
                 </CardHeader>
                     <CardBody>
-                        <img src={this.state.tiles.img} alt="this didn't work"></img>
+
                     </CardBody>
+
+                    <a href="https://app.asana.com/0/home/1199896929625869" target="_blank" rel="noopener noreferrer">
+                <img className="w-25" src={this.state.tiles.img} alt="instagram"/>
+              <div className="pt-2">
+                 Asana
+              </div>
+              </a>
             </Card>
         )
     }
