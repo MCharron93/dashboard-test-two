2864cb7583797b05a532550122f1721d0a631bcf
MCharron93 <michelle.charron93@gmail.com>
2021-03-10 16:38:32 -0700

    pulling in collection imgs and links

---
+++ src/variables/hrtooldata.js
@@ -0,0 +1,29 @@
+import tsheets from "../assets/img/Icon Tiles/t-sheets-logo.jpeg"
+import indeed from "../assets/img/Icon Tiles/indeed-logo.png"
+import ziprecruiter from "../assets/img/Icon Tiles/ziprecruiter-logo.png"
+import gsuitejobs from "../assets/img/Icon Tiles/gmail-logo.png"
+
+const hrTools = {
+    tiles: [
+        {
+            name: "T-Sheets",
+            img: tsheets,
+            link: "https://tsheets.intuit.com/page/login_oii"
+        },
+        {
+            name: "Indeed",
+            img: indeed,
+            link: "https://www.indeed.com/"
+        },
+        {
+            name: "ZipRecruiter",
+            img: ziprecruiter,
+            link: "https://www.ziprecruiter.com/"
+        },
+        {
+            name: "G-Suite Jobs",
+            img: gsuitejobs,
+            link: "https://gmail.com"
+        },
+    ]
+}
